dffc8919f8202a056ef8bdba3c3810a8714562f4
MCsanbu <118032588+MCsanbu@users.noreply.github.com>
2026-09-26 22:08:19 +0800

    feat(gui): redesign main dashboard UI

--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -9,6 +9,8 @@
 #include <vector>
 
 class QLabel;
+class QStackedWidget;
+class QPushButton;
 class QTableWidget;
 
 namespace sanbucraft::gui {
@@ -23,7 +25,27 @@ public:
                QWidget* parent = nullptr);
 
 private:
-    static QTableWidget* createItemTable(const analysis::WorldAnalysis& analysis, QWidget* parent);
+    QWidget* createSidebar();
+    QWidget* createDashboard();
+    QWidget* createPlayersPage();
+    QWidget* createContainersPage();
+    QWidget* createMapPage();
+    QWidget* createAboutPage();
+
+    QPushButton* createNavButton(const QString& text, int index);
+    QTableWidget* createItemTable(const analysis::WorldAnalysis& analysis, QWidget* parent);
+
+    void switchPage(int index);
+
+    minecraft::WorldInfo world_;
+    std::vector<minecraft::Player> players_;
+    std::vector<minecraft::Container> containers_;
+    analysis::WorldAnalysis analysis_;
+
+    QStackedWidget* pages_ = nullptr;
+    std::vector<QPushButton*> navButtons_;
+    QLabel* pageTitle_ = nullptr;
+    QLabel* pageSubtitle_ = nullptr;
 };
 
 }  // namespace sanbucraft::gui
